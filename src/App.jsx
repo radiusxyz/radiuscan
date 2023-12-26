beef1cf6021db7ddde6c7ebc16aae4b98f397c4d
gylman <gylmanbm@gmail.com>
2023-12-26 10:22:00 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css';
+import styled from 'styled-components';
+const M = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+function App() {
+  return <M></M>;
+}
+
+export default App;
