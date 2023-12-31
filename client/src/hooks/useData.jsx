af6737820ca0d33edac8c6119fba2177c81adc9e
gylman <gylmanbm@gmail.com>
2023-12-31 20:44:57 +0900

    add rolex column on the table and request the rollup block txs to display into it

--- client/src/hooks/useData.jsx
+++ client/src/hooks/useData.jsx
@@ -1,14 +1,30 @@
 const users = [
-  '0xF9b77fcf66edBC5aD6EC01368c760EE8Dfc1e697',
-  '0xcB81aB3f5A81420e12f475904E2147a70b4F6682',
-  '0x17720D5A01Be8107BFEbe1bA62cc5d204a858674',
-  '0x2b3a99e2dFA4cB99DAC80d17f7d0Fd72c535Aaf6',
-  '0xA56de31220e562c0299F3C52BebbdFBB62B63647',
-  '0xB7bE96534C2672367d4d10Eb62494d1cE60301Ea',
-  '0x67583e7cD9193bbd965dEd85e21dA01938E7A5Af',
-  '0x166BC2C4aC656ef32691f1F6C642134EE66A6674',
-  '0x86a292E91A4400da46bad3D1C8dfaBAF78F8558B',
-  '0xe33023fe2E3576F69fE8B50ee8Ff280d0A3917D4',
+  '0x9e76f4a22dd5a9bd105433ac5db52308942e44f0',
+  '0xe06428cd274662e336919bd5cc7e660dd6f5f3d0',
+  '0xdac26028ac5479cb0722c8c62ce567b13efb4547',
+  '0x00d4c8c54253277f395e037751818d12c42fd20c',
+  '0x9e979d723cf1c11c7067e973c8d349584f1cd7a6',
+  '0x93be203a03e22b125384d4e4f5c7373da222092f',
+  '0xe43fa6cbb7dda0bd863593c5e75fae9b4dcd5cd4',
+  '0x32cf060fff42ed5a31d5c336800529354f93ac1a',
+  '0xd924c14f40364e692412dffb65924e448de7b20a',
+  '0xccd55bd2fec878dcd749c166e55e42bf41a17d1b',
+  '0xef0c0e8c60fb0b0fba2d1af11be8f5c35de8fca9',
+  '0xef0c0e8c60fb0b0fba2d1af11be8f5c35de8fca9',
+  '0xef0c0e8c60fb0b0fba2d1af11be8f5c35de8fca9',
+  '0x1d0ad4b655d8b49ceba4342765245b90bc4bd7e7',
+  '0x1a1563ab3819180c6156d6de13c393aa8cf8174d',
+  '0x91722db88a8810e2e4ae2e4549aee9eb2b9a4e8a',
+  '0xb834ed914228b68b0e1f3e4f1e168ed2b580a35c',
+  '0x1b59718eafa2bffe5318e07c1c3cb2edde354f9c',
+  '0x41ee28ee05341e7fdddc8d433ba66054cd302ca1',
+  '0xd2301aedf58a6ef7c069de1edef76ece00b7d503',
+  '0x2d623d3dec2f0b0217ccfb689e1642d84ca845fb',
+  '0x5b40a99d6c790158f5c18d5b61bb1c943a633ffe',
+  '0x428ab2ba90eba0a4be7af34c9ac451ab061ac010',
+  '0xf89d7b9c864f589bbf53a82105107622b35eaa40',
+  '0xd1007fb1d5e18f84f262bdfdebc7a450c35aa03f',
+  '0xdeaddeaddeaddeaddeaddeaddeaddeaddead0001',
 ];
 
 const encrypteds = [
@@ -38,63 +54,35 @@ const encrypteds = [
   '0xac70Ccdc079b7d9E47AAd4fb3E13a88565916B69',
   '0x834d5d7eEB11325d98308e03C3E5A3dA823cE33C',
   '0x47f5B352764eF442037543459b112b9d20164775',
-  '0xb262e88565858EA333354A1B9Ac12C9A73eb1ed1',
-  '0x7737AbbA056b2E21f9Ee808b4f8e35a47C1309f6',
-  '0x159609aFEa12461D57a400800eAc2Bd70A220Fa2',
-  '0x9CE436Ea0cF6000856d5764c5eB921d1f73E0CA6',
-  '0x23cE201Ef54027e3fe469A3d0d0EEbB63DCaA55B',
-  '0xDfe52ADc1624d196114F2386E86ac9667bf7c8F5',
-  '0x428b2A3BEDa7570A27Bf733569cc64cB93B94cbf',
-  '0x2C10CeF180c22E179216CF87A665735b9a1b59c5',
-  '0xfbCf7b54b460f5A9dF2C95777d4A4f408CF27d90',
-  '0x056201aFe2698b77ad240F087711f327d2126518',
-  '0x577Ee90857C800c7D8Df05f819D701F83544235F',
-  '0xC06ac23D4aeFF713b5BfB4eAaE4c4D294BD67959',
-  '0xeAbE97E48032F9cf49698ed4A0B9bb88aB1E75Ce',
-  '0x695F79E34972915c849aF90FFb9a211BF603697f',
 ];
 
 const decrypteds = [
-  '0x705BA02F8883741B7bC9704777427eb909b3751f',
-  '0x84Ca2b1c1f7f043Dde531A428Fc765C6637A6f5e',
-  '0xE0bD1Da5a7A1F07d37445507370398f72689B1F7',
-  '0x6BAE181e7AAd23c124db01Bb31a17B4d2C5E2623',
-  '0x55c1644098A152417B7d933815bC2f6ca5626800',
-  '0x3bC7E42EB323c79CF8CaDE6351dE276D7cc62847',
-  '0x7661c0C98B4B122eaC5b9090F0B42037Fd98D0f8',
-  '0xc0249212d476E9C116cCAAe15923D9210d2C00b6',
-  '0x854F3503C167839a415b121002480721c36211Ac',
-  '0xD847C054A0D15A5Acd8bED3648BB8978Bb3D9ef1',
-  '0xc55530BF1124458725B1C5b3472c2190eE24cba6',
-  '0x7DD996d47bE8563E36649A90Eb730eDf9dAcf77a',
-  '0xa0Da6556173C7D8Ed2c8f9201AF4d03E51bc307d',
-  '0x7610EDC7FF8884eB5E8C178c5bAFe20520BCef92',
-  '0x543524DF01b271B37fc6e6ee9C15D375B67BFD25',
-  '0x8f8e40a5b94Ed89F0E1d1e16f03C26f4E3B67c4c',
-  '0xb8c08C9e1c442D45a6CC2F34ccEE524F77B9daB1',
-  '0x3f20D806825DE606aF28F56f0BFa19cAa315771A',
-  '0xD5CC49d69d2238D0fCFBf6a120F4dcE0cceb59d4',
-  '0xAaBb2181EF1CcdF25c9BA1013Fb805628204f1Be',
-  '0xd867a333b6B3F51955a57FB6fEFb5341f438309a',
-  '0x6A8079811D6AFf44F02891B1c342a9FB3f58d763',
-  '0xc73baC7d177ec91dEe33d2B4cE2FC9f16aC74f10',
-  '0x30471bf4A16046FE2b4cD325f0f718949864EC04',
-  '0x11B173e1DB1a0438a6BC8E3b678bD7e4F0F013b3',
-  '0x341dA9d48D7868e664Ee2e1500f0D38cce423bAe',
-  '0x79941148C4ea22c9Ac8B1d661a4D156C5a7b16B8',
-  '0x6051BC74D44Ee15fbeD729F7B2431EE5163bf9f6',
-  '0x53d6cE51dA8444f746Fb49d878Ba05F5edd91a88',
-  '0xEfeb6B559f255e999eF570c0cd35E6246C4f1253',
-  '0x41F82e1Aa813a976d2a5d0594a6B05bcfEC9194B',
-  '0x6ce0A5Da6a2aDfCA5506a785CF481d52c9ea9782',
-  '0x2C25bbda8BA3C44e25235A0B72B110c1eDB66429',
-  '0x8c6b58FA24b97dd34f0F95960bc03117316adB3F',
-  '0xA2A207b08C74E0a15176E717F30fD18DC54D263e',
-  '0x98eB05e8028d219BD8575abf93021141b558f714',
-  '0x722ba76FF54C930AE05cF8d567ebeC226110C1AD',
-  '0x287Ee7419A6a7b379D55facb59c530A3556EFcBd',
-  '0x0F415B200864E47F9f3715015344053679994e4D',
-  '0x8236BB2366d16074a48eAcA7F2De25f2B3009f52',
+  '0xe673b8a9f0e16943e698830c73924503da9a7c87682d0703ffd44522a0416e43',
+  '0x151424bddf90a74a33a18d52203501dba270065dbe0ee516e1b67d64c88940be',
+  '0xb52139b69e04f6bf6a85bea28f258de435b666b54360b3c74010186f26f822d6',
+  '0x53d9861b7fda306ee8602214a09c66d377163546ef54617d5ee6299672239103',
+  '0x425eaac66c6c7c7a8bd90460c88165b026f7b560ef3d38d91ec05adc1cde4ff5',
+  '0xac97ded313c8355cdb09a2a08fee401b882e3bec471d30d45c556f0d04abfece',
+  '0x3665d056b2625e34ea7e70c0c6bbf73580eaf81247f9552315a5a76b0125fb5c',
+  '0xead6ae1eea82d561502fe23fb3237e61a17b6ac974e2e27f0a573a7e41041d2f',
+  '0x07c9f935b85bc5f983e68b84f12725def1a5cb3f5f3fcc4fe22bb462aef28263',
+  '0xb2a4b88a4f1d392aff8e7fa450420243ab353fee75dcf806c18b5bbabfe48d28',
+  '0x44383c4828181ad53423fed08096e67859c4c36e359a2ca709271b63d5211175',
+  '0x739158c7b43df7252c74c3c7c8ed5be6b774e4323029e12ee3748c0b67616fb3',
+  '0xd7c022e42dc6911c3082a86516f1bff48e83e37a431b885db36e7546d7cc4c30',
+  '0xa39eeee24dfdaaa918b39a14868702ca35adf3d6fdc74788874ae31ff411da61',
+  '0xf30a37176095d50ea4da8e43e7fe2178b585e7838e783c4753da4825950fe17a',
+  '0x147974648dfb199e9c8e4ed9181d44ffcffefc2ebc649118fb7f2972c9035b58',
+  '0x4f9aec45b46bf072b978162d7db84a2d0c5dddcc2ff7e992dffa0420d7ddfe81',
+  '0x7c0108416642beeeac61ee9fda0cf554221ab545920c142805cbe98f13381667',
+  '0xd2ef68376c1836141f9404a3ff89854fcc226f69d93d4fca780a9c5266ab9fe9',
+  '0xedb788f18af7eedd305e6d661cf1b587aac30faa2f7a31e352fa15841506e587',
+  '0xfd8ddff13de99974331c3c1b252125cd9eb2e49bbfa4c6e680f12e508bb8949d',
+  '0x324aafe3858c00066bef0912523daaeed555aeba21f2e76e46aa65bf3220d9b4',
+  '0xb6442ea9b7dede559948dfbb477544e718a0d3e679f669b8a348ef1f4687eac1',
+  '0x9bd1ff9806600bfb661e0941ebc89b55d33f0a800ccc6ffeb8fabe8a938ae118',
+  '0x70d48ea4a0bdc30d2f84e56d7c128e26c40c802c552feb118a504e2fb565c430',
+  '0xb765704d843235e2299c229d9eb3f51ab6e123f97b89d6921fa4eeabd26e1231',
 ];
 
 const followers = [
@@ -106,19 +94,9 @@ const followers = [
 
 const blocks = [
   {
-    height: '227185',
+    height: '114125955',
     sequencer: '0x6514D59960CAcA398E51c3C8f9A515a44eB0CE5C',
     signature: '0x281055afc982d96fab65b3a49cac8b878184cb16',
-  },
-  {
-    height: '205364',
-    sequencer: '0x30400A149D97866B8a5a639e48e93A4E08A15705',
-    signature: '0x6f46cf5569aefa1acc1009290c8e043747172d89',
-  },
-  {
-    height: '102032',
-    sequencer: '0x6514D59960CAcA398E51c3C8f9A515a44eB0CE5C',
-    signature: '0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae',
   },
 ];
 
@@ -139,6 +117,191 @@ const rollups = [
   },
 ];
 
+const fromRollup = [
+  {
+    decrypted:
+      '0xe673b8a9f0e16943e698830c73924503da9a7c87682d0703ffd44522a0416e43',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0x9e76f4a22dd5a9bd105433ac5db52308942e44f0',
+    to: '0x20279b6d57ba6d3ef852f34800e43e39d46d6487',
+  },
+  {
+    decrypted:
+      '0x151424bddf90a74a33a18d52203501dba270065dbe0ee516e1b67d64c88940be',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0xe06428cd274662e336919bd5cc7e660dd6f5f3d0',
+    to: '0xb49c4e680174e331cb0a7ff3ab58afc9738d5f8b',
+  },
+  {
+    decrypted:
+      '0xb52139b69e04f6bf6a85bea28f258de435b666b54360b3c74010186f26f822d6',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0xdac26028ac5479cb0722c8c62ce567b13efb4547',
+    to: '0x20279b6d57ba6d3ef852f34800e43e39d46d6487',
+  },
+  {
+    decrypted:
+      '0x53d9861b7fda306ee8602214a09c66d377163546ef54617d5ee6299672239103',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0x00d4c8c54253277f395e037751818d12c42fd20c',
+    to: '0x20279b6d57ba6d3ef852f34800e43e39d46d6487',
+  },
+  {
+    decrypted:
+      '0x425eaac66c6c7c7a8bd90460c88165b026f7b560ef3d38d91ec05adc1cde4ff5',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0x9e979d723cf1c11c7067e973c8d349584f1cd7a6',
+    to: '0x5800249621da520adfdca16da20d8a5fc0f814d8',
+  },
+  {
+    decrypted:
+      '0xac97ded313c8355cdb09a2a08fee401b882e3bec471d30d45c556f0d04abfece',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0x93be203a03e22b125384d4e4f5c7373da222092f',
+    to: '0x9ec1c3dcf667f2035fb4cd2eb42a1566fd54d2b7',
+  },
+  {
+    decrypted:
+      '0x3665d056b2625e34ea7e70c0c6bbf73580eaf81247f9552315a5a76b0125fb5c',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0xe43fa6cbb7dda0bd863593c5e75fae9b4dcd5cd4',
+    to: '0xa062ae8a9c5e11aaa026fc2670b0d65ccc8b2858',
+  },
+  {
+    decrypted:
+      '0xead6ae1eea82d561502fe23fb3237e61a17b6ac974e2e27f0a573a7e41041d2f',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0x32cf060fff42ed5a31d5c336800529354f93ac1a',
+    to: '0x0b2c639c533813f4aa9d7837caf62653d097ff85',
+  },
+  {
+    decrypted:
+      '0x07c9f935b85bc5f983e68b84f12725def1a5cb3f5f3fcc4fe22bb462aef28263',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0xd924c14f40364e692412dffb65924e448de7b20a',
+    to: '0xd85b5e176a30edd1915d6728faebd25669b60d8b',
+  },
+  {
+    decrypted:
+      '0xb2a4b88a4f1d392aff8e7fa450420243ab353fee75dcf806c18b5bbabfe48d28',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0xccd55bd2fec878dcd749c166e55e42bf41a17d1b',
+    to: '0xa062ae8a9c5e11aaa026fc2670b0d65ccc8b2858',
+  },
+  {
+    decrypted:
+      '0x44383c4828181ad53423fed08096e67859c4c36e359a2ca709271b63d5211175',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0xef0c0e8c60fb0b0fba2d1af11be8f5c35de8fca9',
+    to: '0x0563c1d22765674fc4c5e07857eb62394dba5f20',
+  },
+  {
+    decrypted:
+      '0x739158c7b43df7252c74c3c7c8ed5be6b774e4323029e12ee3748c0b67616fb3',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0xef0c0e8c60fb0b0fba2d1af11be8f5c35de8fca9',
+    to: '0x0563c1d22765674fc4c5e07857eb62394dba5f20',
+  },
+  {
+    decrypted:
+      '0xd7c022e42dc6911c3082a86516f1bff48e83e37a431b885db36e7546d7cc4c30',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0xef0c0e8c60fb0b0fba2d1af11be8f5c35de8fca9',
+    to: '0x0563c1d22765674fc4c5e07857eb62394dba5f20',
+  },
+  {
+    decrypted:
+      '0xa39eeee24dfdaaa918b39a14868702ca35adf3d6fdc74788874ae31ff411da61',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0x1d0ad4b655d8b49ceba4342765245b90bc4bd7e7',
+    to: '0xb20f0105f3598652a3be569132f7b3f341106ddc',
+  },
+  {
+    decrypted:
+      '0xf30a37176095d50ea4da8e43e7fe2178b585e7838e783c4753da4825950fe17a',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0x1a1563ab3819180c6156d6de13c393aa8cf8174d',
+    to: '0xa0dbdb0767afdb382322715a85e877b0f4e08fcf',
+  },
+  {
+    decrypted:
+      '0x147974648dfb199e9c8e4ed9181d44ffcffefc2ebc649118fb7f2972c9035b58',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0x91722db88a8810e2e4ae2e4549aee9eb2b9a4e8a',
+    to: '0xdad904e1c8387e0626de1443f112c9d0123e5a03',
+  },
+  {
+    decrypted:
+      '0x4f9aec45b46bf072b978162d7db84a2d0c5dddcc2ff7e992dffa0420d7ddfe81',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0xb834ed914228b68b0e1f3e4f1e168ed2b580a35c',
+    to: '0x584f57911b6eedab5503e202f8e193663c9bd3db',
+  },
+  {
+    decrypted:
+      '0x7c0108416642beeeac61ee9fda0cf554221ab545920c142805cbe98f13381667',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0x1b59718eafa2bffe5318e07c1c3cb2edde354f9c',
+    to: '0x6f26bf09b1c792e3228e5467807a900a503c0281',
+  },
+  {
+    decrypted:
+      '0xd2ef68376c1836141f9404a3ff89854fcc226f69d93d4fca780a9c5266ab9fe9',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0x41ee28ee05341e7fdddc8d433ba66054cd302ca1',
+    to: '0xf9ff53db441a2f6aeff6c36e107dc7f6c5bd0bb5',
+  },
+  {
+    decrypted:
+      '0xedb788f18af7eedd305e6d661cf1b587aac30faa2f7a31e352fa15841506e587',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0xd2301aedf58a6ef7c069de1edef76ece00b7d503',
+    to: '0xdc6ff44d5d932cbd77b52e5612ba0529dc6226f1',
+  },
+  {
+    decrypted:
+      '0xfd8ddff13de99974331c3c1b252125cd9eb2e49bbfa4c6e680f12e508bb8949d',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0x2d623d3dec2f0b0217ccfb689e1642d84ca845fb',
+    to: '0xdc6ff44d5d932cbd77b52e5612ba0529dc6226f1',
+  },
+  {
+    decrypted:
+      '0x324aafe3858c00066bef0912523daaeed555aeba21f2e76e46aa65bf3220d9b4',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0x5b40a99d6c790158f5c18d5b61bb1c943a633ffe',
+    to: '0xf67e42fef26a913cfb6cb24a7fb285fd19d3822a',
+  },
+  {
+    decrypted:
+      '0xb6442ea9b7dede559948dfbb477544e718a0d3e679f669b8a348ef1f4687eac1',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0x428ab2ba90eba0a4be7af34c9ac451ab061ac010',
+    to: '0x6f26bf09b1c792e3228e5467807a900a503c0281',
+  },
+  {
+    decrypted:
+      '0x9bd1ff9806600bfb661e0941ebc89b55d33f0a800ccc6ffeb8fabe8a938ae118',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0xf89d7b9c864f589bbf53a82105107622b35eaa40',
+    to: '0x1bb1905878bfeea40f11da5bd44ce3b74b74a77b',
+  },
+  {
+    decrypted:
+      '0x70d48ea4a0bdc30d2f84e56d7c128e26c40c802c552feb118a504e2fb565c430',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0xd1007fb1d5e18f84f262bdfdebc7a450c35aa03f',
+    to: '0x50bce64397c75488465253c0a034b8097fea6578',
+  },
+  {
+    decrypted:
+      '0xb765704d843235e2299c229d9eb3f51ab6e123f97b89d6921fa4eeabd26e1231',
+    timestamp: '2023-12-29 11:51:27',
+    from: '0xdeaddeaddeaddeaddeaddeaddeaddeaddead0001',
+    to: '0x4200000000000000000000000000000000000015',
+  },
+];
+
 let orders = new Array(encrypteds.length)
   .fill(0)
   .map((_, i) => String(i));
@@ -151,7 +314,7 @@ function useData() {
   const pickNum = (num) => Math.floor(Math.random() * num);
 
   return encrypteds.map((_, index, arr) => {
-    const user = pickMember(users);
+    const user = users[index];
     const encrypted = encrypteds[index];
     const decrypted = decrypteds[index];
     const seqSig = `ecdsa-${pickNum(9999)}`;
--- client/src/pages/BlockTxs.jsx
+++ client/src/pages/BlockTxs.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import Table from '../components/Table';
 import { useTxs } from '../contexts/TxsContext';
+import axios from 'axios';
 
 const BlockTxs = () => {
   const { height } = useParams();
   const { txs } = useTxs();
+  const [entries, setEntries] = useState(
+    txs
+      .filter((tx) => tx.block.height === height)
+      .map(
+        ({
+          encrypted,
+          decrypted,
+          order,
+          status,
+          seqSig,
+          block,
+        }) => {
+          return {
+            encrypted: encrypted,
+            order,
+            'sig for user': seqSig,
+            orderer: block.sequencer,
+            decrypted,
+            'sig for rollup': block.signature,
+            status,
+          };
+        }
+      )
+  );
+
   const headers = [
     'encrypted',
     'order',
@@ -13,31 +39,39 @@ const BlockTxs = () => {
     'orderer',
     'decrypted',
     'sig for rollup',
+    'rolex',
     'status',
   ];
 
-  const entries = txs
-    .filter((tx) => tx.block.height === height)
-    .map(
-      ({
-        encrypted,
-        decrypted,
-        order,
-        status,
-        seqSig,
-        block,
-      }) => {
-        return {
-          encrypted: encrypted,
-          order,
-          'sig for user': seqSig,
-          orderer: block.sequencer,
-          decrypted,
-          'sig for rollup': block.signature,
-          status,
-        };
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const blockNumber = '114125955'; // Replace with dynamic value as needed
+        const response = await axios.get(
+          `http://localhost:3333/rollup/block/${blockNumber}`
+        );
+        setData(response.data);
+        console.log(response.data);
+        setEntries((prevState) => {
+          return prevState.map((entry, index) => ({
+            ...entry,
+            rolex: response.data[index].decrypted,
+          }));
+        });
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError(error);
       }
-    );
+    };
+
+    fetchData();
+  }, []); // Empty dependency array means this runs once after the component mounts
+
+  if (error) return 'An error occurred.';
+  if (!data) return 'Loading...';
 
   return <Table headers={headers} entries={entries} />;
 };
